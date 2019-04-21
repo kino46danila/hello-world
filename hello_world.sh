b61a63469d78418e5934081a16b013ba3756bef1
kino46danila <47347152+kino46danila@users.noreply.github.com>
2019-04-22 01:12:54 +0300

    Add files via upload

---
+++ hello_world.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+sl -l
+clear
+echo "\n \n \n \t \t \t HELLO WORLD!!!"
+sleep 3
---
+++ install.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+apt update -y
+apt upgrade -y
+apt-get install sl -y
+
+./hello_world.sh
